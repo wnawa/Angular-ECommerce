1274c61349bc947ca50de36b59e3b2f99700552d
walaa nawar <walaa_nawar86@yahoo.com>
2024-01-18 11:08:57 +0100

    latest fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,36 +1,29 @@
-import { Component  } from '@angular/core';
+import { Component } from '@angular/core';
+
 import { RouterLink } from '@angular/router';
-import { CarouselModule } from 'ngx-owl-carousel-o';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
-import { OwlOptions } from 'ngx-owl-carousel-o';
-// import './hero-details-box.css';
-import 'rxjs';
 import { LatestProductsComponent } from '../latest-products/latest-products.component';
-import { ViewEncapsulation } from '@angular/compiler';
-declare var $:any;
-// import * as $ from 'jquery';
-// import  "../../assets/css/owl.carousel.css";
-// import  "../../assets/js/owl.carousel.min.js";
-// import $ from "jquery";
-// import jQuery from '@types/jquery';
-// declare global {
-//   interface Window {
-//       jQuery: typeof jQuery;
-//       $: typeof jQuery;
-//   }
-// }
+
+import { DomSanitizer } from '@angular/platform-browser'
+import { CommonModule } from '@angular/common';
+// import { PipeTransform, Pipe } from "@angular/core";
+
+// @Pipe({ name: 'safeHtml'})
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, CarouselModule, SlickCarouselModule,LatestProductsComponent],
+  imports: [
+    RouterLink,
+    CommonModule,
+    SlickCarouselModule,
+    LatestProductsComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  animations: [
-    // animation triggers go here
-  ]
+  
 })
 export class HomeComponent {
-  
+   // Main courasoul configuration
   slides = [
     {
       img: '../../assets/img/h4-slide.png',
@@ -39,7 +32,7 @@ export class HomeComponent {
     },
     {
       img: '../../assets/img/h4-slide2.png',
-      title: 'buy one, get one 50% off',
+      title:`by one, get one <span class="primary">50% <strong>off</strong></span>` ,//'buy one, get one 50% off',
       subtitle: 'school supplies & backpacks.*',
     },
     {
@@ -61,8 +54,9 @@ export class HomeComponent {
     autoplaySpeed: 1500,
     isFinite: true,
     arrows: true,
-  
   };
+
+   // Brands corasoul configuration
   slideConfigLogos = {
     accessibility: true,
     dots: false,
@@ -93,18 +87,58 @@ export class HomeComponent {
           slidesToScroll: 1,
         },
       },
-    ],nextArrow: '<button type="button" class=" logoslick-next ">next</button>',
-    prevArrow: '<button type="button" class="  logoslick-prev  ">prev</button>'}
-  slidesLogos= [
-    {img :"../../assets/img/brand1.png" },
-    {img :"../../assets/img/brand2.png" },
-    {img :"../../assets/img/brand3.png" },
-    {img :"../../assets/img/brand4.png" },
-    {img :"../../assets/img/brand5.png" },
-    {img :"../../assets/img/brand6.png" },
-    {img :"../../assets/img/brand1.png" },
-    {img :"../../assets/img/brand2.png" }
+    ],
+    nextArrow: '<button type="button" class=" logoslick-next ">next</button>',
+    prevArrow: '<button type="button" class="  logoslick-prev  ">prev</button>',
+  };
+  slidesLogos = [
+    { img: '../../assets/img/brand1.png' },
+    { img: '../../assets/img/brand2.png' },
+    { img: '../../assets/img/brand3.png' },
+    { img: '../../assets/img/brand4.png' },
+    { img: '../../assets/img/brand5.png' },
+    { img: '../../assets/img/brand6.png' },
+    { img: '../../assets/img/brand1.png' },
+    { img: '../../assets/img/brand2.png' },
   ];
 
-
+  // Latest products corasoul configuration
+  slideConfiglatestProducts = {
+    accessibility: true,
+    dots: false,
+    slidesToShow: 5,
+    autoplay: false,
+    autoplaySpeed: 1500,
+    isFinite: true,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1008,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+    nextArrow: '<button type="button" class=" logoslick-next ">next</button>',
+    prevArrow: '<button type="button" class="  logoslick-prev  ">prev</button>',
+  };
 }
+function Pipe(arg0: { name: string; }): (target: typeof HomeComponent) => void | typeof HomeComponent {
+  throw new Error('Function not implemented.');
+}
+
